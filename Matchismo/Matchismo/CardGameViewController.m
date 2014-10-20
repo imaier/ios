b5f3660b02c4333d201a6bc5ab98e272114b96d0
Ilya Maier <imaier@avaya.com>
2014-10-20 15:26:39 +0400

    Assignment 2 added support for all orientation and added support for both iPad and iPhone devices

--- Matchismo/Matchismo/CardGameViewController.m
+++ Matchismo/Matchismo/CardGameViewController.m
@@ -17,6 +17,7 @@
 @property (weak, nonatomic) IBOutlet UILabel *scoreLablel;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *gameModeSegmentControl;
 @property (weak, nonatomic) IBOutlet UILabel *lastConsiderationLabel;
+@property (weak, nonatomic) IBOutlet UIButton *aNewGameButton;
 @end
 
 @implementation CardGameViewController
@@ -90,6 +91,93 @@
             break;
     }
     
+}
+
+const NSUInteger upperDownEdgeSpace = 20;
+const NSUInteger leftRightEdgeSpace = 20;
+const NSUInteger cardSpace = 8;
+
+-(void)viewWillLayoutSubviews
+{
+    CGRect cardFrame = CGRectZero;
+    CGRect newFrame = CGRectZero;
+    int rows = 0;
+    int cols = 0;
+
+    
+    BOOL sixColsLayout = YES;
+    
+    if(self.view.bounds.size.width > self.view.bounds.size.height )
+    {
+        sixColsLayout = NO;
+    }
+    
+    if (sixColsLayout == YES) {
+        cols = 6;
+        rows = 5;
+        
+        //gameModeSegmentControl
+        newFrame = self.gameModeSegmentControl.frame;
+        newFrame.origin.y = self.aNewGameButton.frame.origin.y - newFrame.size.height - cardSpace;
+        self.gameModeSegmentControl.frame = newFrame;
+        
+        //aNewGameButton
+        newFrame = self.aNewGameButton.frame;
+        newFrame.origin.x = leftRightEdgeSpace;
+        self.aNewGameButton.frame = newFrame;
+        
+        //lastConsiderationLabel
+        newFrame = self.lastConsiderationLabel.frame;
+        newFrame.origin.y = self.gameModeSegmentControl.frame.origin.y - newFrame.size.height - cardSpace;
+        self.lastConsiderationLabel.frame = newFrame;
+        
+        //scoreLablel
+        newFrame = self.scoreLablel.frame;
+        newFrame.origin.x = self.aNewGameButton.frame.origin.x
+         + self.aNewGameButton.frame.size.width + cardSpace;
+        self.scoreLablel.frame = newFrame;
+        
+        
+        
+    } else {
+        cols = 10;
+        rows = 3;
+        
+        //gameModeSegmentControl
+        newFrame = self.gameModeSegmentControl.frame;
+        newFrame.origin.y = self.aNewGameButton.frame.origin.y + self.aNewGameButton.frame.size.height - newFrame.size.height;
+        self.gameModeSegmentControl.frame = newFrame;
+        
+        //aNewGameButton
+        newFrame = self.aNewGameButton.frame;
+        newFrame.origin.x = self.gameModeSegmentControl.frame.origin.x + self.gameModeSegmentControl.frame.size.width + cardSpace;
+        self.aNewGameButton.frame = newFrame;
+        
+        //lastConsiderationLabel
+        newFrame = self.lastConsiderationLabel.frame;
+        newFrame.origin.y = self.gameModeSegmentControl.frame.origin.y - newFrame.size.height - cardSpace;
+        self.lastConsiderationLabel.frame = newFrame;
+        
+        //scoreLablel
+        newFrame = self.scoreLablel.frame;
+        newFrame.origin.x = self.aNewGameButton.frame.origin.x
+        + self.aNewGameButton.frame.size.width + cardSpace;
+        self.scoreLablel.frame = newFrame;
+    }
+
+    cardFrame.size.width = (self.view.bounds.size.width - 2*leftRightEdgeSpace - cardSpace*(cols-1)) / cols;
+    cardFrame.size.height = cardFrame.size.width * 1.5;
+    
+    int i,j;
+    
+    //card's buttons
+    for (j=0; j<rows; j++) {
+        cardFrame.origin.y =  upperDownEdgeSpace + (cardFrame.size.height + cardSpace) * j;
+        for (i = 0; i< cols; i++) {
+            cardFrame.origin.x =  leftRightEdgeSpace + (cardFrame.size.width + cardSpace) * i;
+            ((UIView*)self.cardButtons[j*cols+i]).frame = cardFrame;
+        }
+    }
 }
 
 @end
